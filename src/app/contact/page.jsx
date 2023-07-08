4df101f3409704713e351ce339335d5a46c8e614
leleduc <leleduc@gmail.com>
2023-07-08 14:58:54 +0700

    First Commit

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './page.module.css';
+
+const Contact = () => {
+  return <div className={style.container}>Contact</div>;
+};
+
+export default Contact;
